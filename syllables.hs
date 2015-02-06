d1ae0842c624790516e271206d379415ea77f905
Joe MacMahon <joe.macmahon@cern.ch>
2015-02-06 15:38:15 +0100

    Valid nuclei

--- syllables.hs
+++ syllables.hs
@@ -6,38 +6,37 @@ module Syllables ( parse
 import Text.ParserCombinators.Parsec
 import Data.Maybe ( isJust, fromMaybe )
 
+import Debug.Trace ( trace )
+
 trousers = "traʊzəz"
-improvements = "ɪmpruːvmənts"
+improvements = "ɪmpru:vmənts"
 glimpsed = "glɪmpst"
-
-vowels :: [Char]
-vowels = "aʊəɪuːə"
-
-consonants :: [Char]
-consonants = "pbtdkgfvθðszʃʒʧʤmnŋlrwjh"
+table = "tɛɪbɫ̩"
+rhythm = "rɪðm̩"
 
 singleOnsets = ["p", "b", "t", "d", "k", "g", "f", "v", "θ", "ð", "s", "z", "ʃ", "ʒ", "ʧ", "ʤ", "m", "n", "l", "r", "w", "j", "h"]
-
 doubleOnsets = ["pl", "pr", "pj", "bl", "br", "bj", "tr", "tj", "tw", "dr", "dj", "dw", "kl", "kr", "kj", "kw", "gl", "gr", "gj", "gw", "mj", "nj", "fl", "fr", "fj", "vj", "θr", "θj", "θw", "sl", "sj", "sw", "sp", "st", "sk", "sm", "sn", "ʃr", "hj", "lj"]
-
 tripleOnsets = ["spl", "spr", "spj", "str", "stj", "skl", "skr", "skj", "skw"]
-
 validOnsets :: [[Char]]
 validOnsets = tripleOnsets ++ doubleOnsets ++ singleOnsets ++ [""]
-
-singleCodas = ["p", "b", "t", "d", "k", "g", "f", "v", "θ", "ð", "s", "z", "ʃ", "ʒ", "ʧ", "ʤ", "m", "n", "ŋ", "l", "r"]
-
-doubleCodas = ["pt", "pθ", "ps", "tθ", "ts", "kt", "ks", "kθ", "bd", "bz", "dz", "gd", "gz", "ʧt", "ʤd", "mp", "md", "mf", "mθ", "mz", "nt", "nd", "nʧ", "nʤ", "nθ", "ns", "nz", "ŋd", "ŋk", "ŋz", "ŋθ", "lp", "lt", "lk", "lb", "ld", "lʧ", "lʤ", "lm", "ln", "lf", "lv", "lθ", "ls", "lz", "lʃ", "ft", "fθ", "fs", "vd", "vz", "θt", "θs", "ðd", "ðz", "sp", "st", "sk", "zd", "ʃt", "ʒd", "rp", "rb", "rt", "rd", "rt", "rʧ", "rʤ", "rk", "rg", "rm", "rn", "rl"]
-
-tripleCodas = ["pts", "pst", "pθs", "tst", "tθs", "dst", "kts", "kst", "ksθ", "kθs", "mpt", "mps", "mfs", "ntθ", "nts", "ndz", "nʧt", "nʤd", "nθs", "nst", "nzd", "ŋkt", "ŋkθ", "ŋks", "lpt", "lps", "lts", "lkt", "lks", "lbz", "ldz", "lʧt", "lʤd", "lmd", "lmz", "lnz", "lfs", "lfθ", "lvd", "lvz", "lθs", "lst", "fts", "fθs", "spt", "sps", "sts", "skt", "sks", "rmθ", "rpt", "rps", "rts", "rst", "rkt"]
-
-quadCodas = ["mpts", "mpst", "lkts", "lpts", "lfθs", "ksts", "ksθs", "ntθs"]
-
-validCodas :: [[Char]]
-validCodas = [""] ++ singleCodas ++ doubleCodas ++ tripleCodas ++ quadCodas
-
 validOnsetsParsers :: [P String]
 validOnsetsParsers = map (try . string) validOnsets
+
+syllabicConsonants = ["ɫ̩", "n̩", "m̩", "r̩"]
+dipthongs = ["ɛɪ", "aɪ", "ɔɪ", "əʊ", "aʊ", "iə", "ɛə", "uə", "eɪ", "ɑɪ", "ʌʊ", "oʊ", "ɛʊ", "eʊ"]
+longVowels = ["i:", "ɑ:", "ɔ:", "u:", "ɜ:", "e:", "o:"]
+shortVowels = ["ɪ", "ɛ", "a", "ʌ", "ɒ", "ʊ", "ə", "i", "e", "o"]
+validNuclei :: [[Char]]
+validNuclei = syllabicConsonants ++ dipthongs ++ longVowels ++ shortVowels
+validNucleiParsers :: [P String]
+validNucleiParsers = map (try . string) validNuclei
+
+singleCodas = ["p", "b", "t", "d", "k", "g", "f", "v", "θ", "ð", "s", "z", "ʃ", "ʒ", "ʧ", "ʤ", "m", "n", "ŋ", "l", "r"]
+doubleCodas = ["pt", "pθ", "ps", "tθ", "ts", "kt", "ks", "kθ", "bd", "bz", "dz", "gd", "gz", "ʧt", "ʤd", "mp", "md", "mf", "mθ", "mz", "nt", "nd", "nʧ", "nʤ", "nθ", "ns", "nz", "ŋd", "ŋk", "ŋz", "ŋθ", "lp", "lt", "lk", "lb", "ld", "lʧ", "lʤ", "lm", "ln", "lf", "lv", "lθ", "ls", "lz", "lʃ", "ft", "fθ", "fs", "vd", "vz", "θt", "θs", "ðd", "ðz", "sp", "st", "sk", "zd", "ʃt", "ʒd", "rp", "rb", "rt", "rd", "rt", "rʧ", "rʤ", "rk", "rg", "rm", "rn", "rl"]
+tripleCodas = ["pts", "pst", "pθs", "tst", "tθs", "dst", "kts", "kst", "ksθ", "kθs", "mpt", "mps", "mfs", "ntθ", "nts", "ndz", "nʧt", "nʤd", "nθs", "nst", "nzd", "ŋkt", "ŋkθ", "ŋks", "lpt", "lps", "lts", "lkt", "lks", "lbz", "ldz", "lʧt", "lʤd", "lmd", "lmz", "lnz", "lfs", "lfθ", "lvd", "lvz", "lθs", "lst", "fts", "fθs", "spt", "sps", "sts", "skt", "sks", "rmθ", "rpt", "rps", "rts", "rst", "rkt"]
+quadCodas = ["mpts", "mpst", "lkts", "lpts", "lfθs", "ksts", "ksθs", "ntθs"]
+validCodas :: [[Char]]
+validCodas = [""] ++ singleCodas ++ doubleCodas ++ tripleCodas ++ quadCodas
 
 data Syllables = Syllables { syllOnset :: String, syllNucleus :: String, syllCoda :: String, syllNext :: Maybe Syllables }
                  deriving (Eq)
@@ -61,9 +60,8 @@ type P = GenParser Char ()
 onset :: P String
 onset = choice validOnsetsParsers
 
--- TODO: dipthongs, syllabic consonants
 nucleus :: P String
-nucleus = many1 (oneOf vowels)
+nucleus = choice validNucleiParsers
 
 syllables :: P Syllables
 syllables = do ons <- onset
