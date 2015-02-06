6cd9705e0c92a5090373696cd1e5075eba8d1ddd
Joe MacMahon <joe.macmahon@cern.ch>
2015-02-06 15:44:19 +0100

    Cleaned up

--- syllables.hs
+++ syllables.hs
@@ -6,14 +6,14 @@ module Syllables ( parse
 import Text.ParserCombinators.Parsec
 import Data.Maybe ( isJust, fromMaybe )
 
-import Debug.Trace ( trace )
-
+-- Some example transcriptions
 trousers = "traʊzəz"
 improvements = "ɪmpru:vmənts"
 glimpsed = "glɪmpst"
 table = "tɛɪbɫ̩"
 rhythm = "rɪðm̩"
 
+-- ONSETS SECTION
 singleOnsets = ["p", "b", "t", "d", "k", "g", "f", "v", "θ", "ð", "s", "z", "ʃ", "ʒ", "ʧ", "ʤ", "m", "n", "l", "r", "w", "j", "h"]
 doubleOnsets = ["pl", "pr", "pj", "bl", "br", "bj", "tr", "tj", "tw", "dr", "dj", "dw", "kl", "kr", "kj", "kw", "gl", "gr", "gj", "gw", "mj", "nj", "fl", "fr", "fj", "vj", "θr", "θj", "θw", "sl", "sj", "sw", "sp", "st", "sk", "sm", "sn", "ʃr", "hj", "lj"]
 tripleOnsets = ["spl", "spr", "spj", "str", "stj", "skl", "skr", "skj", "skw"]
@@ -22,6 +22,7 @@ validOnsets = tripleOnsets ++ doubleOnsets ++ singleOnsets ++ [""]
 validOnsetsParsers :: [P String]
 validOnsetsParsers = map (try . string) validOnsets
 
+-- NUCLEI SECTION
 syllabicConsonants = ["ɫ̩", "n̩", "m̩", "r̩"]
 dipthongs = ["ɛɪ", "aɪ", "ɔɪ", "əʊ", "aʊ", "iə", "ɛə", "uə", "eɪ", "ɑɪ", "ʌʊ", "oʊ", "ɛʊ", "eʊ"]
 longVowels = ["i:", "ɑ:", "ɔ:", "u:", "ɜ:", "e:", "o:"]
@@ -31,6 +32,7 @@ validNuclei = syllabicConsonants ++ dipthongs ++ longVowels ++ shortVowels
 validNucleiParsers :: [P String]
 validNucleiParsers = map (try . string) validNuclei
 
+-- CODAS SECTION
 singleCodas = ["p", "b", "t", "d", "k", "g", "f", "v", "θ", "ð", "s", "z", "ʃ", "ʒ", "ʧ", "ʤ", "m", "n", "ŋ", "l", "r"]
 doubleCodas = ["pt", "pθ", "ps", "tθ", "ts", "kt", "ks", "kθ", "bd", "bz", "dz", "gd", "gz", "ʧt", "ʤd", "mp", "md", "mf", "mθ", "mz", "nt", "nd", "nʧ", "nʤ", "nθ", "ns", "nz", "ŋd", "ŋk", "ŋz", "ŋθ", "lp", "lt", "lk", "lb", "ld", "lʧ", "lʤ", "lm", "ln", "lf", "lv", "lθ", "ls", "lz", "lʃ", "ft", "fθ", "fs", "vd", "vz", "θt", "θs", "ðd", "ðz", "sp", "st", "sk", "zd", "ʃt", "ʒd", "rp", "rb", "rt", "rd", "rt", "rʧ", "rʤ", "rk", "rg", "rm", "rn", "rl"]
 tripleCodas = ["pts", "pst", "pθs", "tst", "tθs", "dst", "kts", "kst", "ksθ", "kθs", "mpt", "mps", "mfs", "ntθ", "nts", "ndz", "nʧt", "nʤd", "nθs", "nst", "nzd", "ŋkt", "ŋkθ", "ŋks", "lpt", "lps", "lts", "lkt", "lks", "lbz", "ldz", "lʧt", "lʤd", "lmd", "lmz", "lnz", "lfs", "lfθ", "lvd", "lvz", "lθs", "lst", "fts", "fθs", "spt", "sps", "sts", "skt", "sks", "rmθ", "rpt", "rps", "rts", "rst", "rkt"]
@@ -38,9 +40,11 @@ quadCodas = ["mpts", "mpst", "lkts", "lpts", "lfθs", "ksts", "ksθs", "ntθs"]
 validCodas :: [[Char]]
 validCodas = [""] ++ singleCodas ++ doubleCodas ++ tripleCodas ++ quadCodas
 
+-- ADT for a Syllable.  Isomorphic to a [(String, String, String)].
 data Syllables = Syllables { syllOnset :: String, syllNucleus :: String, syllCoda :: String, syllNext :: Maybe Syllables }
                  deriving (Eq)
 
+-- Convenience functions for the parser and representations
 parseSyllables :: String -> Either ParseError Syllables
 parseSyllables = parse syllables ""
 
@@ -57,6 +61,8 @@ instance Show Syllables where
 
 type P = GenParser Char ()
 
+
+-- THE PARSER
 onset :: P String
 onset = choice validOnsetsParsers
 
@@ -84,8 +90,3 @@ coda' s = do end <- optionMaybe eof
                          then validCodaParser (s, rest)
                          else do c <- anyChar
                                  coda' (s ++ [c])
-                       
-
--- parsec test
-one :: P String
-one = choice [(try $ (string "test" >>= \s -> eof >> return s)), try (string "testing")]
